19f247307efe8820e205197eb417e8a3cad82588
Setsu-Adachi <setsmob@gmail.com>
2024-02-08 00:36:13 +0000

    adding more employee

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 const team = [];
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
-
-
-
-
-
 // Question for the role.
 const questions = [
     {
@@ -55,7 +49,7 @@ const questions = [
     },
     {
         type: "input",
-        name: "name",
+        name: "github",
         message: "What is your engineer's github user name?",
                 when: function (answers) {
             if (answers.role === "Engineer") {
@@ -66,7 +60,7 @@ const questions = [
     },
     {
         type: "input",
-        name: "name",
+        name: "school",
         message: "What is your intern's school?",
         when: function (answers) {
             if (answers.role === "Intern") {
@@ -76,16 +70,48 @@ const questions = [
         }
     },
 ]
-
+function askQuestion() {
+// Code to gather information about the development team members, and render the HTML file.
 inquirer.prompt(questions) 
 .then(function(answers){
 // Make new empolyee based on the role, New Manager, New Intern, New Eng, 
-// add on the list of the team member
+if (answers.role === "Manager") {
+    const employee = new Manager (answers.name, answers.id, answers.email, answers.number);
+    team.push(employee);
+}
+if (answers.role === "Engineer") {
+    const employee = new Engineer (answers.name, answers.id, answers.email, answers.github);
+    team.push(employee);  
+}
+if (answers.role === "Intern") {
+    const employee = new Intern (answers.name, answers.id, answers.email, answers.school);
+    team.push(employee);  
+}
 
-// ask to add another employee
-})
-.then(function(){
-fs.writeFile(outputPath, render(team));
+// add on the list of the team member and ask to add another employee
+inquirer.prompt([
+    {
+        type: "list",
+        name: "add",
+        message: "Do you want to add another team member?",
+        choices: ["Yes", "No"],
+    },
+])
+.then(
+    function(answers) {
+        if (answers.add === "Yes") {
+    askQuestion();
+    
+        }
+        else{
+            writeFile();
+        }
+    }
+)
 });
+}
 // Output directory
-// 
+function writeFile(){
+    fs.writeFile(outputPath, render(team))
+}
+askQuestion()
