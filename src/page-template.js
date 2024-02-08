57b4691cc0e0d2aa765064fdca4287cf02338b36
Setsu-Adachi <setsmob@gmail.com>
2024-02-08 22:23:31 +0000

    Changed the GitHub rendring clickable

--- src/page-template.js
+++ src/page-template.js
@@ -32,7 +32,7 @@ const generateTeam = team => {
         <ul class="list-group">
             <li class="list-group-item">ID: ${engineer.getId()}</li>
             <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-            <li class="list-group-item">GitHub: ${engineer.getGithub()}</li>
+            <li class="list-group-item">GitHub: https://github.com/${engineer.getGithub()}</li>
         </ul>
     </div>
 </div>
